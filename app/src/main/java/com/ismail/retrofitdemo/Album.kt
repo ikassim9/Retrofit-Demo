0e14cc2a82c8153d823a5b12f173e6bbc14f3bbb
Ismail Kassim <jq3583el@go.minnstate.edu>
2020-08-10 00:05:22 -0500

    add album class to parse json

---
+++ app/src/main/java/com/ismail/retrofitdemo/Album.kt
@@ -0,0 +1,3 @@
+package com.ismail.retrofitdemo
+
+class Album : ArrayList<AlbumItems>()
---
+++ app/src/main/java/com/ismail/retrofitdemo/AlbumItems.kt
@@ -0,0 +1,7 @@
+package com.ismail.retrofitdemo
+
+data class AlbumItems(
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
